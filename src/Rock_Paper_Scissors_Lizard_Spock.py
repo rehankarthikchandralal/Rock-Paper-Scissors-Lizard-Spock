f9dac91084b3b729dcbdb2c242d898322acb4044
rehankarthikchandralal <rehankarthik12@gmail.com>
2025-01-23 00:34:03 +0100

    removed unnecessary docstrings

--- src/Rock_Paper_Scissors_Lizard_Spock.py
+++ src/Rock_Paper_Scissors_Lizard_Spock.py
@@ -1,28 +1,8 @@
 import random
 
-class RPSLS():
-
-    """
-    Rock-paper-scissors-lizard-Spock is a more complex version of the classic hand game 
-    where players choose one of five options: rock, paper, scissors, lizard, or Spock.
-    Instead of each option having only one weakness and one strength,
-    each option beats two other options and loses to two other options.
-    """
+class RPSLS():  
     
 def name_to_number(name):
-
-    """
-    Given string name, returns integer 0, 1, 2, 3, or 4 
-    The key idea of this function is to equate the strings
-    "rock", "paper", "scissors", "lizard", "Spock" to numbers
-    as follows:
-    0 - rock
-    1 - Spock
-    2 - paper
-    3 - lizard
-    4 - scissors
-    """
-
     if(name == "rock"):
         return 0
     elif(name =="Spock"):
@@ -36,15 +16,9 @@ def name_to_number(name):
     else:
         print("ERROR:The input is INVALID")  
         return None
-
-
+        
 def number_to_name(number):
-
-    """
-    Given integer number (0, 1, 2, 3, or 4) is converted to 
-    corresponding name 
-    """
-
+    
     if(number == 0):
         return "rock"
     elif(number == 1):
@@ -59,13 +33,7 @@ def number_to_name(number):
         print("ERROR:The input is INVALID")  
         return None
 
-
 def rpsls(player_choice):
-
-    """
-    Given string player_choice, play a game of RPSLS 
-    and print results to console
-    """
 
     print() 
     print("The player's choice is",player_choice)
@@ -94,7 +62,3 @@ rpsls("Spock")
 rpsls("paper")
 rpsls("lizard")
 rpsls("scissors")
-
-
-#print(name_to_number("spock"))
-#print(number_to_name(5))
